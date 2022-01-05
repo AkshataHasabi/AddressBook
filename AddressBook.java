2a733072e0ab1a78f95d34ddbb51d7012c23ac78
AkshataHasabi <akshuh818@gmail.com>
2022-01-05 23:21:42 +0530

    ADDED:new person contacts are added

--- AddressBook.java
+++ AddressBook.java
@@ -8,6 +8,14 @@ public class AddressBook {
 	System.out.println("welcome to address book");
 	Person arrayOne=new Person("Akshata", "Hasabi", "Yaragatti", "Bangalore", "Karnatak", 591129, 1234567891, "akshata@gmail.com");
 	Person arrayTwo=new Person("Vinayak", "Rajaput", "Savadatti", "Bangalore", "Karnatak", 591126, 987235456, "vinayak@gmail.com");
+	Person arrayThree=new Person();
+	arrayThree.setFirstName("Basavaraj");
+	arrayThree.setLastName("Hasabi");
+	arrayThree.setAddress("Dharwad");
+	arrayThree.setCity("Bangalore");
+	arrayThree.setState("Karnataka");
+	arrayThree.setZip(12345);
+	arrayThree.setPhoneNumber(456851136);
 	
 	//creating array list
 	ArrayList<Person> list=new ArrayList<Person>();
@@ -16,9 +24,14 @@ public class AddressBook {
 	list.add(arrayOne);
 	list.add(arrayTwo);
 	
+	//add new persons details
+	
+	list.add(arrayThree);
+	
 	//printing the array elements
 	for(Person elements : list) {
 		System.out.println("Print the Array:"+ elements);
-	}	
+	}
+	System.out.print("After adding the persons contacts:"+list);
   }
 }
